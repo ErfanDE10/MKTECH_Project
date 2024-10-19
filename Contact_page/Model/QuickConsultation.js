3f429f36c95a860808274bf9b2a0ef2c4a5462ac
Erfan Zarabadi pour <150175418+ErfanDE10@users.noreply.github.com>
2024-10-19 23:47:46 +0330

    Update QuickConsultation.js

--- Contact_page/Model/QuickConsultation.js
+++ Contact_page/Model/QuickConsultation.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 
 const quickConsultSchema = new mongoose.Schema({
   phone: { type: String, required: true },
-  requestType: { type: String, default: 'quick consultation' },  // Default request type
-  lang: { type: String, default: 'en' }  // Default to English if not provided
+  requestType: { type: String, default: 'quick consultation' }, 
+  lang: { type: String, default: 'en' }  
 }, { timestamps: true })
 
 module.exports = mongoose.model('QuickConsult', quickConsultSchema)
